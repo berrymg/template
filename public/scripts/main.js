0a28896a7e8bc5d6fdee38d11485941f48241a9c
berrymg <berrymg@rose-hulman.edu>
2025-07-29 12:04:43 -0400

    update... trying to fix weird git stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * @fileoverview
  * Provides the JavaScript interactions for all pages.
  *
@@ -10,27 +10,81 @@
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_MOVIEQUOTES = "MovieQuotes";
+rhit.FB_KEY_QUOTE = "quote";
+rhit.FB_KEY_MOVIE = "movie";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbMovieQuotesManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
-
+		console.log("created ListPageController")
 	}
-
-	methodName() {
-
+	updateList() {}
+   }
+   
+   rhit.MovieQuote = class {
+	constructor(id, quote, movie) {
+	  this.id = id;
+	  this.quote = quote;
+	  this.movie = movie;  
 	}
-}
+   }
+   
+   rhit.FbMovieQuotesManager = class {
+	constructor() {
+	console.log("created FbMovieQuotesManager")
+	  this._documentSnapshots = [];
+	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTES);
+	}
+	add(quote, movie) {}
+	beginListening(changeListener) {}
+	stopListening() {}
+	update(id, quote, movie) {}
+	delete(id) {}
+	get length() {}
+	getMovieQuoteAtIndex(index) {}
+   }
+   
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+
+	if(document.querySelector("#listPage")) {
+		console.log("You are on the list page.")
+		rhit.fvMovieQuoteManager = new rhit.FbMovieQuotesManager();
+		new rhit.ListPageController
+
+
+
+	}
+
+
+	if(document.querySelector("#detailPage")) {
+		console.log("You are on the detail page.")
+		new rhit.ListPageController
+	}
+
+
+	// //temp code for read and add
+	// const ref = firebase.firestore().collection("MovieQuotes");
+	// ref.onSnapshot((quearySnapshot) => {
+
+	// 	quearySnapshot.forEach((doc) => {
+	// 		console.log(doc.data());
+	// 	});
+	// });
+
+	// ref.add({
+	// 	quote: "My first test",
+	// 	movie: "My first movie"
+	// });
+
 };
+
+
+
 
 rhit.main();
